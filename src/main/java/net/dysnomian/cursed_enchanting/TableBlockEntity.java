0cea27511ae3f91b50dd75e9f1b00b38267ea8ce
DysnomianC <Dysnomian.gamer@gmail.com>
2020-11-27 00:09:32 +1300

    Added fancy book rendering to cursed enchanting table

--- src/main/java/net/dysnomian/cursed_enchanting/TableBlockEntity.java
+++ src/main/java/net/dysnomian/cursed_enchanting/TableBlockEntity.java
@@ -1,5 +1,7 @@
 package net.dysnomian.cursed_enchanting;
 
+import java.util.Random;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
@@ -14,17 +16,89 @@ import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Tickable;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.MathHelper;
 
-public class TableBlockEntity extends BlockEntity implements ImplementedInventory, NamedScreenHandlerFactory {
+public class TableBlockEntity extends BlockEntity implements ImplementedInventory, NamedScreenHandlerFactory, Tickable {
    public static final Identifier ID = new Identifier("cursed_enchanting", "cursed_enchanting_table");
    public static final BlockEntityType<TableBlockEntity> BLOCK_ENTITY_TYPE = BlockEntityType.Builder
          .create(TableBlockEntity::new, TableBlock.INSTANCE).build(null);
 
    private final DefaultedList<ItemStack> items = DefaultedList.ofSize(2, ItemStack.EMPTY);
+   private static final Random RANDOM = new Random();
+
+   public final boolean GO_FOR_FANCY = RANDOM.nextBoolean();
 
    public TableBlockEntity() {
       super(BLOCK_ENTITY_TYPE);
+   }
+
+   // fields and tick method copied from minecraft's `EnchantingTableBlockEntity`, used 
+   public int ticks;
+   public float nextPageAngle;
+   public float pageAngle;
+   public float field_11969;
+   public float field_11967;
+   public float nextPageTurningSpeed;
+   public float pageTurningSpeed;
+   public float field_11964;
+   public float field_11963;
+   public float field_11962;
+   public void tick() {
+      this.pageTurningSpeed = this.nextPageTurningSpeed;
+      this.field_11963 = this.field_11964;
+      PlayerEntity playerEntity = this.world.getClosestPlayer((double)this.pos.getX() + 0.5D, (double)this.pos.getY() + 0.5D, (double)this.pos.getZ() + 0.5D, 3.0D, false);
+      if (playerEntity != null) {
+         double d = playerEntity.getX() - ((double)this.pos.getX() + 0.5D);
+         double e = playerEntity.getZ() - ((double)this.pos.getZ() + 0.5D);
+         this.field_11962 = (float)MathHelper.atan2(e, d);
+         this.nextPageTurningSpeed += 0.1F;
+         if (this.nextPageTurningSpeed < 0.5F || RANDOM.nextInt(40) == 0) {
+            float f = this.field_11969;
+
+            do {
+               this.field_11969 += (float)(RANDOM.nextInt(4) - RANDOM.nextInt(4));
+            } while(f == this.field_11969);
+         }
+      } else {
+         this.field_11962 += 0.02F;
+         this.nextPageTurningSpeed -= 0.1F;
+      }
+
+      while(this.field_11964 >= 3.1415927F) {
+         this.field_11964 -= 6.2831855F;
+      }
+
+      while(this.field_11964 < -3.1415927F) {
+         this.field_11964 += 6.2831855F;
+      }
+
+      while(this.field_11962 >= 3.1415927F) {
+         this.field_11962 -= 6.2831855F;
+      }
+
+      while(this.field_11962 < -3.1415927F) {
+         this.field_11962 += 6.2831855F;
+      }
+
+      float g;
+      for(g = this.field_11962 - this.field_11964; g >= 3.1415927F; g -= 6.2831855F) {
+      }
+
+      while(g < -3.1415927F) {
+         g += 6.2831855F;
+      }
+
+      this.field_11964 += g * 0.4F;
+      this.nextPageTurningSpeed = MathHelper.clamp(this.nextPageTurningSpeed, 0.0F, 1.0F);
+      ++this.ticks;
+      this.pageAngle = this.nextPageAngle;
+      float h = (this.field_11969 - this.nextPageAngle) * 0.4F;
+      float i = 0.2F;
+      h = MathHelper.clamp(h, -0.2F, 0.2F);
+      this.field_11967 += (h - this.field_11967) * 0.9F;
+      this.nextPageAngle += this.field_11967;
    }
 
    @Override
--- src/main/java/net/dysnomian/cursed_enchanting/TableBlockEntityRenderer.java
+++ src/main/java/net/dysnomian/cursed_enchanting/TableBlockEntityRenderer.java
@@ -1,15 +1,22 @@
 package net.dysnomian.cursed_enchanting;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.entity.model.BookModel;
 import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 
 public class TableBlockEntityRenderer extends BlockEntityRenderer<TableBlockEntity> {
 
@@ -21,20 +28,20 @@ public class TableBlockEntityRenderer extends BlockEntityRenderer<TableBlockEnti
  
     @Override
 	public void render(TableBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+		if (blockEntity.GO_FOR_FANCY) {
+			renderFancyBook(blockEntity, tickDelta, matrices, vertexConsumers, light, overlay);
+		}
+		else {
+			renderBoringBook(blockEntity, tickDelta, matrices, vertexConsumers, light, overlay);
+		}
+		
+	}
+
+	private void renderBoringBook(TableBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
 		matrices.push();
 		
 		final double time = blockEntity.getWorld().getTime() + tickDelta;
-		manipulateRenderMatices(time, matrices);
   
-		// use the light from one block above instead of from inside our block.
-		int lightAbove = WorldRenderer.getLightmapCoordinates(blockEntity.getWorld(), blockEntity.getPos().up());
-		MinecraftClient.getInstance().getItemRenderer().renderItem(bookStack, ModelTransformation.Mode.GROUND, lightAbove, overlay, matrices, vertexConsumers);
-  
-		// Mandatory call after GL calls
-		matrices.pop();
-	 }
-
-	private void manipulateRenderMatices(double time, MatrixStack matrices) {
 		final float x = 0.5f;
 		final float y = 0.95f;
 		final float z = 0.5f;
@@ -51,5 +58,48 @@ public class TableBlockEntityRenderer extends BlockEntityRenderer<TableBlockEnti
 		// rotate (about y axis)
 		final float angle = (float)(time * rotation_speed_factor);
 		matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(angle));
+
+		// use the light from one block above instead of from inside our block.
+		int lightAbove = WorldRenderer.getLightmapCoordinates(blockEntity.getWorld(), blockEntity.getPos().up());
+		MinecraftClient.getInstance().getItemRenderer().renderItem(bookStack, ModelTransformation.Mode.GROUND, lightAbove, overlay, matrices, vertexConsumers);
+  
+		// Mandatory call after GL calls
+		matrices.pop();
 	}
+	
+	private final BookModel book = new BookModel();
+
+	public static final SpriteIdentifier BOOK_TEXTURE;
+
+	// copied from minecraft's EnchantingTableBlockEntityRenderer
+	public void renderFancyBook(TableBlockEntity tableBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j) {
+		  matrixStack.push();
+		  matrixStack.translate(0.5D, 0.75D, 0.5D);
+		  float g = (float)tableBlockEntity.ticks + f;
+		  matrixStack.translate(0.0D, (double)(0.1F + MathHelper.sin(g * 0.1F) * 0.01F), 0.0D);
+	
+		  float h;
+		  for(h = tableBlockEntity.field_11964 - tableBlockEntity.field_11963; h >= 3.1415927F; h -= 6.2831855F) {
+		  }
+	
+		  while(h < -3.1415927F) {
+			 h += 6.2831855F;
+		  }
+	
+		  float k = tableBlockEntity.field_11963 + h * f;
+		  matrixStack.multiply(Vector3f.POSITIVE_Y.getRadialQuaternion(-k));
+		  matrixStack.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(80.0F));
+		  float l = MathHelper.lerp(f, tableBlockEntity.pageAngle, tableBlockEntity.nextPageAngle);
+		  float m = MathHelper.fractionalPart(l + 0.25F) * 1.6F - 0.3F;
+		  float n = MathHelper.fractionalPart(l + 0.75F) * 1.6F - 0.3F;
+		  float o = MathHelper.lerp(f, tableBlockEntity.pageTurningSpeed, tableBlockEntity.nextPageTurningSpeed);
+		  this.book.setPageAngles(g, MathHelper.clamp(m, 0.0F, 1.0F), MathHelper.clamp(n, 0.0F, 1.0F), o);
+		  VertexConsumer vertexConsumer = BOOK_TEXTURE.getVertexConsumer(vertexConsumerProvider, RenderLayer::getEntitySolid);
+		  this.book.method_24184(matrixStack, vertexConsumer, i, j, 1.0F, 1.0F, 1.0F, 1.0F);
+		  matrixStack.pop();
+	   }
+	
+	   static {
+		  BOOK_TEXTURE = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier("entity/enchanting_table_book"));
+	   }
 }
